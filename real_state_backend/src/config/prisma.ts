d6cc284243c06fa57d341a2e6b9406d5d41c6ae5
udayempire <kuday0726@gmail.com>
2026-01-24 14:39:52 +0530

    chore: dep,build fixes

--- real_state_backend/src/config/prisma.ts
+++ real_state_backend/src/config/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client.js'
+import { PrismaClient } from '../../prisma/generated/client.js'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
