9d4bf3b3008f73ef734fd0558bb0cd1cccea35ed
udayempire <kuday0726@gmail.com>
2026-02-05 17:41:17 +0530

    feat: property fetching routes

--- real_state_backend/src/controllers/properties/property.controller.ts
+++ real_state_backend/src/controllers/properties/property.controller.ts
@@ -3,6 +3,10 @@ import { Request, Response } from "express";
 import { addPropertySchema } from "../../validators/property.validators";
 import z from "zod";
 
+type Params = {
+    id: string;
+};
+
 export async function addProperty(req: Request, res: Response) {
     try {
         const userId = req.user?.id;
@@ -38,3 +42,74 @@ export async function addProperty(req: Request, res: Response) {
         return res.status(500).json({ message: "Internal server Error" })
     }
 }
+//Get All Properties
+export async function getAllProperties(req: Request, res: Response) {
+    try {
+        const page = Number(req.query.page ?? 1);
+        const limit = Number(req.query.limit ?? 10);
+        const skip = (page - 1) * limit
+        const [properties, total] = await Promise.all([
+            prisma.property.findMany({
+                where: { status: "ACTIVE" },
+                skip,
+                take: limit,
+                orderBy: { createdAt: "desc" },
+                include: { media: true },
+            }),
+            prisma.property.count({
+                where: { status: "ACTIVE" },
+            }),
+        ]);
+        return res.status(200).json({ data: properties, pagination: { total, page, limit, totalPages: Math.ceil(total / limit) } })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internval server Error" })
+    }
+}   
+//get my properties
+
+export async function getMyProperties(req: Request, res: Response) {
+    try {
+        const userId = req.user?.id;
+        if (!userId) {
+            res.status(401).json({ message: "Unauthorized" })
+        };
+        const properties = await prisma.property.findMany({
+            where: { userId: userId },
+            include: {
+                media: true
+            },
+            orderBy: { createdAt: "desc" },
+        });
+        if (properties.length === 0) {
+            return res.status(200).json({
+                data: [],
+                message: "No properties found",
+            });
+        }
+        return res.status(200).json({ data: properties });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Interval server error" })
+    }
+}
+
+//get specific properties
+export async function getProperty(req: Request<Params>, res: Response) {
+    try {
+        const { id } = req.params;
+        const property = await prisma.property.findUnique({
+            where: { id },
+            include: { media: true }
+        });
+        if (!property) {
+            return res.status(404).json({ message: "No property found for the property id" })
+        }
+        return res.status(200).json({ data: property })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Interval server error" })
+    }
+}
+
--- real_state_backend/src/routes/property/property.route.ts
+++ real_state_backend/src/routes/property/property.route.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { authMiddleware } from "../../middleware/auth";
-import { addProperty } from "../../controllers/properties/property.controller";
+import { addProperty, getAllProperties, getMyProperties, getProperty } from "../../controllers/properties/property.controller";
 import { validate } from "../../middleware/validate";
 import { addPropertySchema } from "../../validators/property.validators";
 
 const router = express.Router();
 
 router.post("/",authMiddleware,validate(addPropertySchema), addProperty);
+router.get("/",authMiddleware, getAllProperties);
+router.get("/my-properties",authMiddleware, getMyProperties);
+router.get("/:id",authMiddleware, getProperty);
 
 export default router;
