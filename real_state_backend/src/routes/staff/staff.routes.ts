d0de4d9d886342dd59dfc394b531577f44608c1e
udayempire <kuday0726@gmail.com>
2026-03-06 16:09:51 +0530

    feat: add request endpoint for gem allocation

--- real_state_backend/src/routes/staff/staff.routes.ts
+++ real_state_backend/src/routes/staff/staff.routes.ts
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 import staffManagementRoutes from "./staff.management";
 import staffAuthRoutes from "./staff.auth";
+import staffGemsRoutes from "./gems.routes";
 router.use('/auth', staffAuthRoutes);
 router.use('/management', staffManagementRoutes);
+router.use('/gems', staffGemsRoutes);
 
 export default router;
