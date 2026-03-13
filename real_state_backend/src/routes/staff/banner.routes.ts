5ad40ae4378faaeeee8571d968cf7fdfef1b7eb6
udayempire <kuday0726@gmail.com>
2026-03-13 12:16:38 +0530

    feat: add auth in banners

--- real_state_backend/src/routes/staff/banner.routes.ts
+++ real_state_backend/src/routes/staff/banner.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { createBanner, getAllBanners, deleteBanner, updateBannerStatus } from "../../controllers/staff/banner.staff.controller";
+import { authMiddleware } from "../../middleware/auth";
+import { requireAdminOrSuperAdmin } from "../../middleware/staff";
 const router = express.Router();
 
-router.post('/', createBanner);
-router.get('/', getAllBanners);
-router.put('/:id/status', updateBannerStatus);
-router.delete('/:id', deleteBanner);
+router.post('/',authMiddleware, requireAdminOrSuperAdmin, createBanner);
+router.get('/',authMiddleware, requireAdminOrSuperAdmin, getAllBanners);
+router.put('/:id/status', authMiddleware, requireAdminOrSuperAdmin, updateBannerStatus);
+router.delete('/:id',authMiddleware, requireAdminOrSuperAdmin, deleteBanner);
 export default router;
